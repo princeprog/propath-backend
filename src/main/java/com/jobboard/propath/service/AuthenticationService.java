4f2c0ad615272efa6e28a72a7389c70a2def0ec5
alprince <132316008+princeprog@users.noreply.github.com>
2025-09-09 11:03:23 +0800

    adding a jwt

---
+++ src/main/java/com/jobboard/propath/service/AuthenticationService.java
@@ -0,0 +1,56 @@
+package com.jobboard.propath.service;
+
+import com.jobboard.propath.dto.LoginUserDto;
+import com.jobboard.propath.dto.RegisterUserDto;
+import com.jobboard.propath.entity.User;
+import com.jobboard.propath.repostitory.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder,
+            JwtService jwtService
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+    }
+
+    public User signup(RegisterUserDto input) {
+        User user = new User()
+                .setPhone(input.getPhone())
+                .setFirstname(input.getFirstname())
+                .setLastname(input.getLastname())
+                .setEmail(input.getEmail())
+                .setPassword(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public String authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        User user = userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+
+        // Generate JWT token
+        return jwtService.generateToken(user);
+    }
+}
---
+++ src/main/java/com/jobboard/propath/service/UserService.java
@@ -0,0 +1,25 @@
+package com.jobboard.propath.service;
+
+import com.jobboard.propath.entity.User;
+import com.jobboard.propath.repostitory.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}
