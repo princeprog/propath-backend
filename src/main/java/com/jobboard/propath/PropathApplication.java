39b0f70201c9d6ff9c133aa6b8fdf9617d6af208
alprince <132316008+princeprog@users.noreply.github.com>
2025-09-09 09:32:10 +0800

    first commit

---
+++ src/main/java/com/jobboard/propath/PropathApplication.java
@@ -0,0 +1,13 @@
+package com.jobboard.propath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobboard/propath/PropathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobboard.propath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
